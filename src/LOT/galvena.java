7cd15c228a5d3c2a258fd55b17d9841445093cda
[Emils Glotovs] <[emilsglotovs@sk.lvt.lvgit config user.emailgit config --global user.email [emilsgltoovs@sk.lvt.lv]git config user.emailgit config --global use.email[emils.glotovs@sk.lvt.lv>
2025-12-16 09:12:20 +0200

    Programmas sakuma izveide

--- src/LOT/galvena.java
+++ src/LOT/galvena.java
@@ -1,9 +1,31 @@
 package LOT;
+
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class galvena {
 
 	public static void main(String[] args) {
+		String izvele;
+		Stack<Integer> LAIMIGAIS = new Stack<>();
+		Stack<Integer> NELAIMIGAIS = new Stack<>();
+		Stack<Integer> NELAIMIGAIS1 = new Stack<>();
 		
-
+		String[] darbibas = {"Veikt izlozi", "Apskatīt pēdējo rezultātu", "iziet"};
+		do {
+			izvele = (String)JOptionPane.showInputDialog(null, "Izvēlies darbību", "darbību saraksts",
+					JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
+			switch(izvele) {
+			case "Veikt izlozi":
+				break;
+			case "Apskatīt pēdējo rezultātu":
+				break;
+			case "iziet":
+				break;
+			}
+break;
+		}while((!izvele.equals("Apturet")));
 	}
 
 }
