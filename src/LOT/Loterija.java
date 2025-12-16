4eb68c1fda723e28f1de06c4de3274f99ca9775b
[Emils Glotovs] <[emilsglotovs@sk.lvt.lvgit config user.emailgit config --global user.email [emilsgltoovs@sk.lvt.lv]git config user.emailgit config --global use.email[emils.glotovs@sk.lvt.lv>
2025-12-16 09:54:46 +0200

    1. globala izmaiņa. Partaisu programmu un taisu Laiku un nejaušus skaitļus Loterijas klasē

--- src/LOT/Loterija.java
+++ src/LOT/Loterija.java
@@ -1,9 +1,45 @@
 package LOT;
 
+import java.beans.IntrospectionException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.Stack;
+
 public class Loterija {
 
-	public static void main(String[] args) {
-		
-	}
 
+		private Stack<Integer> SkS;
+		private Stack<Integer> LSkS;
+		private LocalDateTime ILaiks;
+		
+		public Loterija() {
+			 SkS = new Stack<>();
+			 LSkS = new Stack<>();
+			 aizpilditStack();
+	}
+		private void aizpilditStack() {
+			Random rand = new Random();
+			for (int i = 0; i < 10; i ++) {
+				SkS.push(rand.nextInt(10));
+			}
+		}
+
+		public void VeiktIzlozi() {
+			for (int i = 0; i < 3; i++) {
+				if (SkS.isEmpty()) {
+					LSkS.push(SkS.pop());
+				}
+			}
+		ILaiks = LocalDateTime.now();
+		}
+		
+		public Stack<Integer> getLSkS() {
+			return LSkS;
+		}
+		
+		public LocalDateTime getILaiks() {
+			return ILaiks;
+		}
+		
 }
--- src/LOT/galvena.java
+++ src/LOT/galvena.java
@@ -1,16 +1,20 @@
 package LOT;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JOptionPane;
 
 public class galvena {
-
+Random rand = new Random();
 	public static void main(String[] args) {
 		String izvele;
-		Stack<Integer> LAIMIGAIS = new Stack<>();
-		Stack<Integer> NELAIMIGAIS = new Stack<>();
-		Stack<Integer> NELAIMIGAIS1 = new Stack<>();
+		
+		
+		public Loterija() {
+			
+		}
 		
 		String[] darbibas = {"Veikt izlozi", "Apskatīt pēdējo rezultātu", "iziet"};
 		do {
@@ -18,10 +22,15 @@ public class galvena {
 					JOptionPane.QUESTION_MESSAGE, null, darbibas, darbibas[0]);
 			switch(izvele) {
 			case "Veikt izlozi":
+				if(!S1.isEmpty())
+					
+				
 				break;
 			case "Apskatīt pēdējo rezultātu":
+				
 				break;
 			case "iziet":
+				
 				break;
 			}
 break;
