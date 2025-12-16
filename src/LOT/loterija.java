c578dadacb3df00533eab17473d1a161cfdc9d25
[Emils Glotovs] <[emilsglotovs@sk.lvt.lvgit config user.emailgit config --global user.email [emilsgltoovs@sk.lvt.lv]git config user.emailgit config --global use.email[emils.glotovs@sk.lvt.lv>
2025-12-16 10:27:05 +0200

    Partaists kods, nonemu nevajadzigo

--- src/LOT/loterija.java
+++ src/LOT/loterija.java
@@ -1,137 +1,160 @@
 package LOT;
-import javax.swing.JOptionPane;
+
 import java.util.Stack;
 import java.util.Random;
 import java.time.LocalDateTime;
 import java.time.Duration;
+import javax.swing.JOptionPane;
 
 public class loterija {
 
-    static Stack<Integer> skaitluSteks = new Stack<>();
-    static Stack<Integer> laimigoSteks = new Stack<>();
-    static LocalDateTime pedejasIzlozesLaiks = null;
-
     public static void main(String[] args) {
 
-        while (true) {
+        String izvele;
+        Stack<Integer> skaitluSteks = new Stack<>();
+        Stack<Integer> laimigoSteks = new Stack<>();
+        LocalDateTime pedejasIzlozesLaiks = null;
 
-            String izvele = JOptionPane.showInputDialog(
-                    "Izvēlies darbību:\n" +
-                    "1 - Veikt izlozi\n" +
-                    "2 - Apskatīt visus skaitļus\n" +
-                    "3 - Apskatīt laimīgos skaitļus\n" +
-                    "4 - Iziet"
+        String[] darbibas = {
+                "Veikt izlozi",
+                "Apskatīt visus skaitļus",
+                "Apskatīt laimīgos skaitļus",
+                "Apturēt"
+        };
+
+        do {
+            izvele = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Izvēlies darbību",
+                    "Darbību saraksts",
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    darbibas,
+                    darbibas[0]
             );
 
-            if (izvele == null || izvele.equals("4")) {
-                JOptionPane.showMessageDialog(null, "Programma pabeigta.");
-                break;
+            if (izvele == null) {
+                izvele = "Apturēt";
             }
 
             switch (izvele) {
-                case "1":
-                    veiktIzlozi();
-                    break;
-                case "2":
-                    apskatitVisusSkaitlus();
-                    break;
-                case "3":
-                    apskatitLaimigos();
-                    break;
-                default:
+
+               
+                case "Veikt izlozi":
+
+                    if (pedejasIzlozesLaiks != null) {
+                        Duration starpiba = Duration.between(pedejasIzlozesLaiks, LocalDateTime.now());
+                        if (starpiba.toSeconds() < 60) {
+                            JOptionPane.showMessageDialog(
+                                    null,
+                                    "Izloze iespējama tikai pēc 1 minūtes!",
+                                    "Brīdinājums",
+                                    JOptionPane.WARNING_MESSAGE
+                            );
+                            break;
+                        }
+                    }
+
+                    skaitluSteks.clear();
+                    laimigoSteks.clear();
+
+                    Random random = new Random();
+
+                    for (int i = 0; i < 10; i++) {
+                        skaitluSteks.push(random.nextInt(10));
+                    }
+
                     JOptionPane.showMessageDialog(
                             null,
-                            "Nepareiza izvēle.",
-                            "Kļūda",
-                            JOptionPane.ERROR_MESSAGE
+                            "Steks aizpildīts ar 10 nejaušiem skaitļiem.",
+                            "Paziņojums",
+                            JOptionPane.INFORMATION_MESSAGE
                     );
+
+                    for (int i = 0; i < 3; i++) {
+                        laimigoSteks.push(skaitluSteks.pop());
+                    }
+
+                    pedejasIzlozesLaiks = LocalDateTime.now();
+
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Izloze pabeigta.\nIzlozes laiks:\n" + pedejasIzlozesLaiks,
+                            "Paziņojums",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                    break;
+
+       
+                case "Apskatīt visus skaitļus":
+
+                    if (skaitluSteks.isEmpty()) {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Steks ir tukšs!",
+                                "Paziņojums",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );
+                    } else {
+                        String teksts = "Skaitļi, kas palika stekā:\n";
+
+                        Stack<Integer> kopija = new Stack<>();
+                        kopija.addAll(skaitluSteks);
+
+                        while (!kopija.isEmpty()) {
+                            teksts += kopija.pop() + "\n";
+                        }
+
+                        JOptionPane.showMessageDialog(
+                                null,
+                                teksts,
+                                "Paziņojums",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );
+                    }
+                    break;
+
+   
+                case "Apskatīt laimīgos skaitļus":
+
+                    if (laimigoSteks.isEmpty()) {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Laimīgie skaitļi vēl nav noteikti!",
+                                "Paziņojums",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );
+                    } else {
+                        String teksts = "Laimīgie skaitļi:\n";
+                        teksts += "Izlozes laiks:\n" + pedejasIzlozesLaiks + "\n\n";
+
+                        Stack<Integer> kopija = new Stack<>();
+                        kopija.addAll(laimigoSteks);
+
+                        while (!kopija.isEmpty()) {
+                            teksts += kopija.pop() + "\n";
+                        }
+
+                        JOptionPane.showMessageDialog(
+                                null,
+                                teksts,
+                                "Paziņojums",
+                                JOptionPane.INFORMATION_MESSAGE
+                        );
+                    }
+                    break;
+
+         
+                case "Apturēt":
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Programma apturēta.",
+                            "Paziņojums",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                    break;
             }
-        }
-    }
 
-    static void veiktIzlozi() {
-
-        if (pedejasIzlozesLaiks != null) {
-            Duration starpiba = Duration.between(pedejasIzlozesLaiks, LocalDateTime.now());
-            if (starpiba.toSeconds() < 60) {
-                JOptionPane.showMessageDialog(
-                        null,
-                        "Izloze iespējama tikai pēc 1 minūtes.",
-                        "Informācija",
-                        JOptionPane.INFORMATION_MESSAGE
-                );
-                return;
-            }
-        }
-
-        skaitluSteks.clear();
-        laimigoSteks.clear();
-
-        Random gadijumaSkaitlis = new Random();
-
-        for (int skaititajs = 0; skaititajs < 10; skaititajs++) {
-            skaitluSteks.push(gadijumaSkaitlis.nextInt(10));
-        }
-
-        JOptionPane.showMessageDialog(
-                null,
-                "Steks aizpildīts ar 10 nejaušiem skaitļiem.\nIzloze tiek veikta."
-        );
-
-        for (int skaititajs = 0; skaititajs < 3; skaititajs++) {
-            laimigoSteks.push(skaitluSteks.pop());
-        }
-
-        pedejasIzlozesLaiks = LocalDateTime.now();
-
-        JOptionPane.showMessageDialog(
-                null,
-                "Izloze pabeigta.\nIzlozes laiks:\n" + pedejasIzlozesLaiks
-        );
-    }
-
-    static void apskatitVisusSkaitlus() {
-
-        if (skaitluSteks.empty()) {
-            JOptionPane.showMessageDialog(
-                    null,
-                    "Nav datu. Vispirms veic izlozi."
-            );
-            return;
-        }
-
-        String teksts = "Skaitļi, kas palika stekā:\n";
-
-        Stack<Integer> kopija = new Stack<>();
-        kopija.addAll(skaitluSteks);
-
-        while (!kopija.empty()) {
-            teksts += kopija.pop() + "\n";
-        }
-
-        JOptionPane.showMessageDialog(null, teksts);
-    }
-
-    static void apskatitLaimigos() {
-
-        if (laimigoSteks.empty()) {
-            JOptionPane.showMessageDialog(
-                    null,
-                    "Laimīgie skaitļi vēl nav noteikti."
-            );
-            return;
-        }
-
-        String teksts = "Laimīgie skaitļi:\n";
-        teksts += "Izlozes laiks:\n" + pedejasIzlozesLaiks + "\n\n";
-
-        Stack<Integer> kopija = new Stack<>();
-        kopija.addAll(laimigoSteks);
-
-        while (!kopija.empty()) {
-            teksts += kopija.pop() + "\n";
-        }
-
-        JOptionPane.showMessageDialog(null, teksts);
+        } while (!izvele.equals("Apturēt"));
     }
 }
